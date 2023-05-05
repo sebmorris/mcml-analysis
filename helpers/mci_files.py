20a25880a325c177d7517c23dbc724b46be7e7a7
Sebastian Morris <seb.morris@btinternet.com>
2023-05-05 14:29:13 +0100

    making progress on abc method

--- helpers/mci_files.py
+++ helpers/mci_files.py
@@ -6,7 +6,7 @@ create_layer_dict = lambda n, mu_a, mu_s, g, d: {
     "d": d
 }
 
-def createMciString(layers, file_names):
+def createMciString(layers, file_names, number_photons=1e7):
     def createRunString(layers, output_file_name, n_air=1.4, number_photons=1e7, bin_size=0.05, bin_count=100):
         format_layer_string = lambda layer: f"{layer['n']} {layer['mu_a']} {layer['mu_s']} {layer['g']} {layer['d']}"
 
@@ -21,7 +21,7 @@ def createMciString(layers, file_names):
         run += str(n_air)
         return run
 
-    runStrings = [createRunString(l, f) for l, f in zip(layers, file_names)]
+    runStrings = [createRunString(l, f, number_photons=number_photons) for l, f in zip(layers, file_names)]
 
     output = "1.0 \n" + str(len(layers)) + '\n\n\n'
     output += "\n\n".join(runStrings)
